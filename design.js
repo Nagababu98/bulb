408cce4152fcd15e80cf2c8a9b685bba39603375
Nagababu98 <163400313+Nagababu98@users.noreply.github.com>
2024-03-18 16:15:00 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,8 @@
+function bulboff()
+{
+    document.getElementById("img").src="https://tse1.mm.bing.net/th?id=OIP.VM6NojU3svt85CtR3u4J0AHaHk&pid=Api&P=0&h=180"
+}
+function bulbon()
+{
+    document.getElementById("img").src="https://tse4.mm.bing.net/th?id=OIP.g6ELgpMBfTe6728vdTQw0wHaHk&pid=Api&P=0&h=180"
+}
